0fe25db3f0a5433b275caa735c3c567673999542
Iulian Moisa <iulian.moisa@gmail.com>
2020-11-08 11:05:56 +0100

    initial sg_flutter_mobile_app

---
+++ README.md
@@ -0,0 +1,5 @@
+# sg_flutter_mobile_app
+
+POC for grammar analysis tool on mobile
+
+
---
+++ android/app/src/main/java/com/im/sg_flutter_mobile_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.im.sg_flutter_mobile_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
